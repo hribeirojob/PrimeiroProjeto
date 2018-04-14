0ea92c07de68ff536e0aefc54593923a8ff6134b
Hugo Jeferson <hribeiro.job@gmail.com>
2018-04-14 11:14:05 -0300

    Atividade de Casa do dia 07/04/18

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Pagina2Page } from '../pagina2/pagina2';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  public novaMsg: string;
+  public varBoasvindas: string;
+  
   constructor(public navCtrl: NavController) {
-
+   
+  }
+  ionViewDidLoad(){
+    this.varBoasvindas = 'Bem vindo aos nosso cursos compactos!'
   }
 
+  altVar(){
+    this.varBoasvindas = 'Olá '+ this.novaMsg
+  }
+
+  irPag2(){
+    this.navCtrl.push(Pagina2Page)
+  }
+  
 }
